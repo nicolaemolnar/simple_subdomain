1fc8b6f0513a80a764bc40d6b3032c79c215eac6
Nicolae Alexandru Molnar <73540438+nicolaemolnar@users.noreply.github.com>
2023-04-28 11:50:42 +0200

    Update subdomain_enum.py

--- subdomain_enum.py
+++ subdomain_enum.py
@@ -4,12 +4,15 @@ import requests
 
 import argparse
 
+VERSION = 0.1
+
 def parse_args():
     """
     Parse command line arguments
     """
     parser = argparse.ArgumentParser(description="Subdomain Enumerator")
     parser.add_argument("-d", "--domain", help="Domain to enumerate subdomains for", required=True)
+    parser.add_argument("-v", "--version", help="Retrieve the version of the actual script", action="store_true", required=False)
     return parser.parse_args()
 
 def get_subdomains_crt(domain):
@@ -27,7 +30,11 @@ def get_subdomains_crt(domain):
 
 if __name__ == '__main__':
     args = parse_args()
-
+    
+    if args.v:
+        print(VERSION)
+        exit(0)
+    
     try:
         subdomains = get_subdomains_crt(args.domain)
         if subdomains:
